10a9985d100a43f79b772cf32ac223944eaf073b
Popeydanielmate <14269@coderacademy.edu.au>
2025-10-25 15:56:33 +1100

    feat: main content implemented with text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import "./styles/global.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <main className="main-content">
+        <Home />
+      </main>
     </div>
   );
 }
 
 export default App;
+
 
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../styles/global.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h2 className="movie-section-title">All Movies A–Z</h2>
+      {/* Movie content will go here later */}
+    </div>
+  );
+};
+
+export default Home;
