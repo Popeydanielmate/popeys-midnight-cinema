461d8c695d7b2bf59b2373e5a0caba72fafe28a5
Popeydanielmate <14269@coderacademy.edu.au>
2025-11-15 15:51:47 +1100

    feat: kaspar hauser (1974) added to library

--- src/data/movies.js
+++ src/data/movies.js
@@ -2,6 +2,8 @@ import deathstalkerPoster from "../assets/deathstalker-poster.jpeg";
 import deathstalkerModalPhoto from "../assets/deathstalker_modalcover.png";
 import stalker_modalcover from "../assets/stalker_modalcover.avif";
 import stalkerPoster from "../assets/stalkerPoster.jpg";
+import kasparPoster from "../assets/kasparPoster.jpg";
+import kaspar_modalcover from "../assets/kaspar_modalcover.avif";
 
 const movies = [
   {
@@ -32,11 +34,30 @@ const movies = [
     whyWatch: "One of the most atmospheric and visually striking films ever made, immersing you in a mysterious, haunting world that feels almost alive. It’s philosophical without being pretentious, exploring universal themes like desire, purpose, and fear through mood rather than exposition. The film’s slow, deliberate pacing rewards attention, its imagery is unforgettable, and its emotional core is surprisingly powerful. It’s also hugely influential — shaping modern sci-fi, games, and filmmaking — making it a unique experience that lingers with you long after it ends.",
     posterUrl: stalkerPoster,
     rating: "8.0/10", 
-    genres: ["Dystopian Sci Fi, ", "Psychological Drama ", "Epic", ],
+    genres: ["Dystopian Sci Fi, ", " Psychological Drama, ", " Epic", ],
     duration: "162 min",
     language: "Russian (with English subs) ",
     link: "https://www.youtube.com/watch?v=Q3hBLv-HLEc",
+    },
+
+    {
+    id: 3,
+    title: "The Enigma of Kaspar Hauser",
+    coverPhoto: kaspar_modalcover,
+    year: 1974,
+    director: "Werner Herzog",
+    cast: ["Bruno S", "Walter Ladengast", "Brigitte Mira",],
+    synopsis: "Herzog's film is based upon the true and mysterious story of Kaspar Hauser, a young man who suddenly appeared in Nuremberg in 1828, barely able to talk or walk, and bearing a strange note; he later explained that he had been held captive in a dungeon of some sort for his entire life that he could remember, and only recently was he released, for reasons unknown. His benefactor attempts to integrate him into society, with intriguing results",
+    whyWatch: "one of Werner Herzog’s most haunting and human films, telling the true story of a young man who spent his life in isolation and is suddenly thrust into society. Through Kaspar’s innocent, literal-minded view of the world, the film becomes a beautiful exploration of what it actually means to be “civilized,” how society treats outsiders, and how fragile and strange human behavior really is. Bruno S. gives a raw, unforgettable performance that feels almost documentary-real, and Herzog turns the story into something both philosophical and deeply emotional. It’s a film that stays simple on the surface but resonates long afterward.",
+    posterUrl: kasparPoster,
+    rating: "7.7/10", 
+    genres: ["Period Drama, ", " Biography, ", " History", ],
+    duration: "110 min",
+    language: "German (with English auto sub option) ",
+    link: "https://www.youtube.com/watch?v=t5q6QmiikM8",
     }
+
+
 
 
 ];
